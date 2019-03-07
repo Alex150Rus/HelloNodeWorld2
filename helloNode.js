e3a461d63636dbcc98ce3e20c989aa3fd88ea589
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2019-03-07 22:21:03 +0300

    more examples of NodeJS

--- helloNode.js
+++ helloNode.js
@@ -1,1 +1,11 @@
+const converter = require('./currency.js');
+const Currency = require('./currencyConverter/CurrencyClass');
+
 console.log("HelloNodeWorld!");
+const CanadianDollar = converter.USToCanadian(50);
+
+console.log (`50 USD are equal to ${CanadianDollar} canadian dollars`);
+
+const canadianDollars = 50;
+converterObject = new Currency(0.91);
+console.log(`${canadianDollars} канадских долларов эквивалентно ${converterObject.canadianToUS(canadianDollars)} USD`);
